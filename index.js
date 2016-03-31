94a38acea902f9ee43bfecc8b6193282a32a00d2
gregalan hill <gregalan hill>
2016-03-31 18:52:28 -0500

    Adding the ability to view next search result with 'more' keyword

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ var google = require('google');
 
 google.resultsPerPage = 1;
 var nextcounter = 0;
+var lastSearch = "";
 
 
 var bot = new builder.BotConnectorBot({appId: 'YourAppId', appSecret: 'YourAppSecret'});
 bot.add('/', function(session) {
-	google(session.message.text, function( err, res) {
-		if( err ) console.error(err);
-		session.send(res.links[0].title + ' - ' + res.links[0].href);
-	});
+	var regex = /more/i;
+	var isMore = regex.exec(session.message.text);
+	if( isMore == null) {
+		google(session.message.text, function( err, res) {
+			if( err ) console.error(err);
+			session.send(res.links[0].title + ' - ' + res.links[0].href);
+		});
+		lastSearch = session.message.text;
+		nextcounter = 0;
+	} else {
+		nextcounter += 1;
+		google.resultsPerPage = nextcounter + 1;
+		google(lastSearch, function( err, res) {
+			if( err ) console.error(err);
+			session.send(res.links[nextcounter].title + ' - ' + res.links[nextcounter].href);
+		});
+	}
+
 	/*
 	var re = /sold/i
 	var isSold = re.exec(session.message.text);
